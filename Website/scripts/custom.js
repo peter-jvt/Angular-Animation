b666dae508e866e6ee1055d9e3129e7c14499cee
Peter Hofman <p.hofman@uniconnect.nl>
2015-02-24 23:33:40 +0100

    js recreated

--- Website/scripts/custom.js
+++ Website/scripts/custom.js
@@ -1,1 +1,39 @@
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+
+
+; (function ($) {
+    'use strict';
+    var $body = $('html, body'),
+        content = $('#main').smoothState({
+            // Runs when a link has been activated
+            onStart: {
+                duration: 250, // Duration of our animation
+                render: function (url, $container) {
+                    // toggleAnimationClass() is a public method
+                    // for restarting css animations with a class
+                   // content.toggleAnimationClass('is-exiting');
+                    // Scroll user to the top
+                    $body.animate({
+                        scrollTop: 0
+                    });
+                }
+            },
+            onEnd: {
+                duration: 0, // Duration of the animations, if any.
+                render: function (url, $container, $content) {
+                    $body.css('cursor', 'auto');
+                    $body.find('a').css('cursor', 'auto');
+
+
+                    $('#contents').addClass("is-exiting")
+
+                    //$container.html($content);
+                    $('#contentsB').html($content.find('#content').html());
+
+                    $('#contentsB').addClass("is-entering")
+
+
+                }
+            }
+        }).data('smoothState');
+    //.data('smoothState') makes public methods available
+})(jQuery);
